bb96fe66838f139baa97f7452f45a8a5b7836ae1
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-01-08 17:18:49 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import HandTracker from "./components/HandTracker";
 function App() {
   return (
     <div style={{ background: "#0f172a", height: "100vh" }}>
-      <h1 style={{ color: "white", textAlign: "center", padding: "10px" }}>
-        🖐️ Control por Gestos con IA
-      </h1>
+      <h2 style={{ color: "white", textAlign: "center", padding: "10px" }}>
+        Control por Gestos con IA
+      </h2>
       <HandTracker />
     </div>
   );
