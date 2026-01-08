e041bd7ab6de19b0aedeb25b865d2925a40b268b
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-01-08 16:27:40 -0500

    Update useHandTracker.js

--- src/hooks/useHandTracker.js
+++ src/hooks/useHandTracker.js
@@ -1,4 +1,4 @@
-// hooks/useHandTracker.js
+// src/hooks/useHandTracker.js
 import { useEffect, useCallback } from "react";
 import { Hands } from "@mediapipe/hands";
 import { Camera } from "@mediapipe/camera_utils";
@@ -6,34 +6,37 @@ import { detectGesture } from "../utils/handGestures";
 import { drawLandmarks, drawConnectors } from "../utils/drawHand";
 
 export const useHandTracker = (videoRef, canvasRef) => {
-  const onResults = useCallback((results) => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
+  const onResults = useCallback(
+    (results) => {
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext("2d");
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(results.image, 0, 0, canvas.width, canvas.height);
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.drawImage(results.image, 0, 0, canvas.width, canvas.height);
 
-    let gesture = "Sin mano";
+      let gesture = "Sin mano";
 
-    if (results.multiHandLandmarks) {
-      for (const l of results.multiHandLandmarks) {
-        drawConnectors(ctx, l);
-        drawLandmarks(ctx, l);
-        gesture = detectGesture(l);
+      if (results.multiHandLandmarks) {
+        for (const l of results.multiHandLandmarks) {
+          drawConnectors(ctx, l);
+          drawLandmarks(ctx, l);
+          gesture = detectGesture(l);
+        }
       }
-    }
 
-    ctx.font = "bold 32px Segoe UI, Arial";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.lineWidth = 4;
+      ctx.font = "bold 32px Segoe UI, Arial";
+      ctx.textAlign = "center";
+      ctx.textBaseline = "middle";
+      ctx.lineWidth = 4;
 
-    ctx.strokeStyle = "#000";
-    ctx.strokeText(gesture, canvas.width / 2, 48);
+      ctx.strokeStyle = "#000";
+      ctx.strokeText(gesture, canvas.width / 2, 48);
 
-    ctx.fillStyle = "#fff";
-    ctx.fillText(gesture, canvas.width / 2, 48);
-  }, []);
+      ctx.fillStyle = "#fff";
+      ctx.fillText(gesture, canvas.width / 2, 48);
+    },
+    [canvasRef]
+  );
 
   useEffect(() => {
     const hands = new Hands({
@@ -59,7 +62,6 @@ export const useHandTracker = (videoRef, canvasRef) => {
     });
 
     camera.start();
-
     return () => camera.stop();
   }, [onResults, videoRef]);
 };
